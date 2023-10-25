1f8413d60cc168ee4ed8f21b4265237e5913891e
lovevyas <99319528+lovevyas@users.noreply.github.com>
2023-10-25 14:30:48 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello Love vyas</h1>
+      <h2>good to meet you today </h2>
+    </div>
+  );
+}
